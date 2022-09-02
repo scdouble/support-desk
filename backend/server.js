d347f34930a56c09d30b60b5d64c46bc65c35de0
Tatsu sei <doubledragontaz@gmail.com>
2022-09-02 13:15:34 +0900

    basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server....');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
