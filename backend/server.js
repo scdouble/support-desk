c9f1030a77c56f5f652bc7056f85af3cfa50af14
Tatsu sei <doubledragontaz@gmail.com>
2022-09-03 19:53:22 +0900

    Register user

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const { errorHandle } = require('./middleware/errorMiddleware')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const { errorHandle } = require('./middleware/errorMiddleware')
+
+connectDB()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.send('Hello')
 })
@@ -14,7 +19,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandle)
-
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
