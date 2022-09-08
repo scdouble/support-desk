c49506adbf61375eb8d0314b557f0985cfc5f75a
Tatsu sei <doubledragontaz@gmail.com>
2022-09-08 15:57:51 +0900

    Register User

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { register } from '../features/auth/authSlice'
+import { register, reset } from '../features/auth/authSlice'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -14,7 +16,22 @@ function Register() {
 
   const { name, email, password, password2 } = formData
   const dispatch = useDispatch()
-  const { user, isLoading, isSuccess, message } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    // Redirect when logged in
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   function onChange(e) {
     setFormData((prevState) => {
